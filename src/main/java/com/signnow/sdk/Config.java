7567e2e8892bd57f14594a61063e9dd58ffa2c82
BhanuPrakash <bkaranam@barracuda.com>
2014-12-03 04:08:54 +0530

    commting the changes

--- src/main/java/com/signnow/sdk/Config.java
+++ src/main/java/com/signnow/sdk/Config.java
@@ -2,33 +2,57 @@ package com.signnow.sdk;
 
 import org.apache.commons.codec.binary.Base64;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * Created by Bhanu on 6/26/2014.
  *
  * This class is used to read the configuration parameters like, base endpointURL, clientID, clientSecret to access SignNow Application.
  */
 public class Config {
-    private Config() {
-    }
 
+    static String apiBase;
+    static String clientId;
+    static  String clientSecret;
+    public Config(String apiBase, String clientId, String clientSecret) {
+
+         this.apiBase=apiBase;
+         this.clientId = clientId;
+         this.clientSecret=clientSecret;
+    }
     public static String getApiBase() {
-        // TODO needs to be configurable
-        return "https://capi-eval.signnow.com/api";
+
+        if(apiBase.equals("signnow.eval"))
+        {
+            return "https://capi-eval.signnow.com/api";
+        }
+        else if(apiBase.equals("signnow.public"))
+        {
+            return "https://api.signnow.com";
+        }
+        return apiBase;
     }
 
     public static String getClientId() {
-        // TODO needs to be configurable
-        return "0fccdbc73581ca0f9bf8c379e6a96813";
+            return clientId;
     }
 
     public static String getClientSecret() {
-        // TODO needs to be configurable
-        return "3719a124bcfc03c534d4f5c05b5a196b";
+        return clientSecret;
     }
 
     public static String getBase64EncodedClientCredentials() {
         String idAndSecret = Config.getClientId() + ":" + Config.getClientSecret();
         byte[] encodedCredentials = Base64.encodeBase64(idAndSecret.getBytes());
         return new String(encodedCredentials);
+    }
+
+    public static void main(String args[]){
+
     }
 }
